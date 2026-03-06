07bf16c06712dff9dee034407204b4646eef6769
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-07 04:49:02 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,17 @@
+function registerShop(){
 
+let shop = {
+
+name: document.getElementById("name").value,
+phone: document.getElementById("phone").value,
+address: document.getElementById("address").value,
+email: document.getElementById("email").value,
+wallet: document.getElementById("wallet").value
+
+};
+
+console.log(shop);
+
+alert("Đăng ký thành công");
+
+}
