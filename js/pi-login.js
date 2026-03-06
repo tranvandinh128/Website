1515972f5eca53e05795b1c28896a59b1d2c4abd
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-07 04:47:16 +0700

    Update pi-login.js

--- js/pi-login.js
+++ js/pi-login.js
@@ -1,1 +1,19 @@
-Create pi-login.js
+const Pi = window.Pi;
+Pi.init({ version: "2.0" });
+
+function loginPi() {
+
+Pi.authenticate(['username','payments'], onIncompletePaymentFound)
+.then(function(auth) {
+
+localStorage.setItem("pi_user", auth.user.username);
+
+window.location.href="dashboard.html";
+
+});
+
+}
+
+function onIncompletePaymentFound(payment) {
+console.log(payment);
+}
