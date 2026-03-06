e0452aa92559d3fee896efc5124cb66428a01bbe
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-07 05:06:55 +0700

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,44 +1,8 @@
-let user = localStorage.getItem("pi_user");
+function loadWallet(){
 
-document.getElementById("user").innerHTML =
-"Xin chào: " + user;
+let user = localStorage.getItem("piUser");
 
-let balance = 0;
-
-function deposit(){
-
-let amount = Number(
-document.getElementById("deposit").value
-);
-
-balance = balance + amount;
-
-alert("Nạp thành công");
-
-}
-
-function withdraw(){
-
-let amount = Number(
-document.getElementById("withdraw").value
-);
-
-let fee = 0.05;
-
-let total = amount + fee;
-
-if(balance >= total){
-
-balance = balance - total;
-
-alert("Rút thành công. Phí 0.05 Pi");
-
-}
-
-else{
-
-alert("Không đủ Pi");
-
-}
+document.getElementById("wallet").innerHTML =
+"Ví Pi của: " + user;
 
 }
